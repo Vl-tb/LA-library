7794be8e087415a2bb18ee0328ceb18ad66125e4
Vladyslav Protsenko <vladyslav.protsenko@ucu.edu.ua>
2022-04-12 16:29:34 +0300

    First commit

---
+++ LAlibrary/cmake-build-release/CMakeFiles/la-library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/la-library.dir/library.cpp.o"
+  "libla-library.dylib"
+  "libla-library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/la-library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAlibrary/cmake-build-release/CMakeFiles/la-library.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for la-library.
+# This may be replaced when dependencies are built.
