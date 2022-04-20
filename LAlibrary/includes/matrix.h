dc562454c757de9fc15b27792754113894aba751
Daria Kuzmina <73224682+ivddorrka@users.noreply.github.com>
2022-04-20 23:15:47 +0000

    added some methods

--- LAlibrary/includes/matrix.h
+++ LAlibrary/includes/matrix.h
@@ -37,12 +37,15 @@ public:
         vc1.fill(0);
         std::vector<Vector<T>> vec;
         for (size_t i = 0; i < rows; ++i){
+            for (size_t j=0; j<cols; ++j){
+                vc1[j] = mtrx[i][j];
+            }
             vec.push_back(vc1);
         }
         Vector mtRx(std::vector<Vector<T>>{vec});
         shape = std::vector<int> {rows, cols};
         matrix = mtRx;
-        
+
     }
 
     Matrix(const Matrix &) = default;
@@ -52,6 +55,7 @@ public:
     ~Matrix() = default;
 
     Matrix<T> &operator+(const Matrix<T> &);
+
     Matrix<T> &operator-(const Matrix<T> &);
     Matrix<T> &operator==(const Matrix<T> &);
     Matrix<T> &operator*(T koef);
@@ -64,10 +68,43 @@ public:
     Matrix<T> &transpose();
     Matrix<T> &det();
     Matrix<T> &fill(T num);
-    bool add_comp(const Matrix<T> &);
-    bool mul_comp(const Matrix<T> &);
-    bool isnull();
-    bool isident();
+    bool add_comp(const Matrix<T> &mt){
+        if(shape[0]==mt.shape[0] && shape[1]==mt.shape[1]){
+            return true;
+        }
+        return false;
+    }
+    bool mul_comp(const Matrix<T> &mt){
+        if(shape[1]==mt.shape[0]){
+            return true;
+        }
+        return false;
+    }
+    bool isnull(){
+        for (size_t i =0; i < shape[0]; ++i){
+            if (!matrix[i].isnull()){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isident(){
+        for ( size_t i =0; i < shape[0]; ++i ){
+            for (size_t j=0; j < shape[1]; ++j){
+
+                if(i==j){
+                    if (matrix[i][j]!=1){
+                        return false;
+                    }
+                } else if (matrix[i][j]!=0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    
     T minor(int col, int row);
     T cofactor(int col, int row);
 
